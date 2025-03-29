1766a1a8a0ed26186177358a78acbe50ab318a50
mkpatel-247 <meet.codes247@gmail.com>
2025-03-30 00:51:05 +0530

    Added social media card component.

---
+++ src/app/cards/social/social.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+
+@Component({
+  selector: 'app-social',
+  standalone: true,
+  imports: [],
+  templateUrl: './social.component.html',
+  styleUrl: './social.component.css',
+})
+export class SocialComponent {}
